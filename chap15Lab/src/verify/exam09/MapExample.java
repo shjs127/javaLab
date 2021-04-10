0e11193a33e3f36dddc3b40068e55c290d707e4f
shjs127 <shjs127@hanmail.net>
2021-04-10 21:32:23 +0900

    chap15Lab 과제 9,10

--- chap15Lab/src/verify/exam09/MapExample.java
+++ chap15Lab/src/verify/exam09/MapExample.java
@@ -1,6 +1,7 @@
 package verify.exam09;
 
 import java.util.HashMap;
+import java.util.*;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,24 +11,51 @@ public class MapExample {
 		map.put("blue", 96);
 		map.put("hong", 86);
 		map.put("white", 92);
-		
 		String name = null;
 		int maxScore = 0;
 		int totalScore = 0;
-		
-		Set<Map.Entry<String,Integer>> entrySet = map.entrySet();
-		for(Map.Entry<String,Integer> entry : entrySet) {
-			if(entry.getValue()>maxScore) {
-				name = entry.getKey();
-				maxScore = entry.getValue();
+		// #1 작성 위치
+		int avg=0;
+		/*
+		Set<String> keyset=map.keySet();
+		Iterator<String> keyiterator=keyset.iterator();
+		while (keyiterator.hasNext()) {
+			String key=keyiterator.next();
+			Integer value=map.get(key);
+			totalScore+=value;
+			if(maxScore<value) {
+				maxScore=value;
 			}
-			totalScore += entry.getValue();
+			if(maxScore==value) {
+				name=key;
+			}
 		}
-		
-		int avgScore = totalScore / map.size();
-		System.out.println("평균점수: " + avgScore);
-		
-		System.out.println("최고점수: " + maxScore);
-		System.out.println("최고점수를 받은 아이디: " + name);
-	}
+		avg=totalScore/map.size();	
+		System.out.println("평균점수 : "+avg);
+		System.out.println("최고점수 : " +maxScore);
+		System.out.println("최고점수를 받은 아이디 : "+name);
+		*/
+		Set<Map.Entry<String, Integer>> set=map.entrySet();
+		Iterator<Map.Entry<String, Integer>> iterator=set.iterator();
+		while(iterator.hasNext()) {
+			Map.Entry<String, Integer> entry=iterator.next();
+			String key=entry.getKey();
+			Integer value=entry.getValue();
+			totalScore+=value;
+			if(maxScore<value) {
+				maxScore=value;
+				name=key;
+			}
+		}
+		avg=totalScore/map.size();	
+		System.out.println("평균점수 : "+avg);
+		System.out.println("최고점수 : " +maxScore);
+		System.out.println("최고점수를 받은 아이디 : "+name);
+		}
+
 }
+//실행 결과
+//평균점수: 91
+//최고점수: 96
+//최고점수를 받은 아이디: blue
+//평균 점수를 출력하고, 최고 점수와 최고 점수를 받은 아이디를 출력
--- chap15Lab/src/verify/exam10/TreeSetExample.java
+++ chap15Lab/src/verify/exam10/TreeSetExample.java
@@ -8,9 +8,12 @@ public class TreeSetExample {
 		treeSet.add(new Student("blue", 96));
 		treeSet.add(new Student("hong", 86));
 		treeSet.add(new Student("white", 92));
-		
 		Student student = treeSet.last();
 		System.out.println("최고점수: " + student.score);
 		System.out.println("최고점수를 받은 아이디: " + student.id);
 	}
 }
+		// 실행 결과
+		// 최고점수: 96
+		// 최고점수를 받은 아이디: blue
+
