c2be12a1d2def7fb380b7c5ff72021002a5b4733
shjs127 <shjs127@hanmail.net>
2021-05-05 18:36:48 +0900

    K번째수

--- programmers/src/programmers/K번째수.java
+++ programmers/src/programmers/K번째수.java
@@ -1,5 +1,23 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class K¹øÂ°¼ö {
+	public static void main(String[] args) {
+			int[] a1= {1,5,2,6,3,7,4};
+			int[][] com= {{2,5,3},{4,4,1},{1,7,3}};
+			
+			int[] answer;
+			answer=solution(a1,com);
+	}
+
+	public static int[] solution(int[] array, int[][] commands) {
+		int[] answer = {};
+		int[] arr1=Arrays.copyOfRange(array, commands[0][0], commands[0][1]);
+		answer[0]=Arrays.binarySearch(arr1,commands[0][2]);
+		
+		
+		return answer;
+	}
 
 }
