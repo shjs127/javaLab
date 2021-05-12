6d7ab4d4b0dbfd0ed03a173a797fedc2b1446c00
shjs127 <shjs127@hanmail.net>
2021-05-12 18:34:13 +0900

    체육복

--- programmers/src/programmers/K번째수.java
+++ programmers/src/programmers/K번째수.java
@@ -4,19 +4,22 @@ import java.util.Arrays;
 
 public class K¹øÂ°¼ö {
 	public static void main(String[] args) {
-			int[] a1= {1,5,2,6,3,7,4};
-			int[][] com= {{2,5,3},{4,4,1},{1,7,3}};
-			
-			int[] answer;
-			answer=solution(a1,com);
+		int[] a1 = { 1, 5, 2, 6, 3, 7, 4 };
+		int[][] com = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+
+		int[] answer;
+		answer = solution(a1, com);
 	}
 
 	public static int[] solution(int[] array, int[][] commands) {
-		int[] answer = {};
-		int[] arr1=Arrays.copyOfRange(array, commands[0][0], commands[0][1]);
-		answer[0]=Arrays.binarySearch(arr1,commands[0][2]);
-		
-		
+		int[] answer = new int[commands.length];
+		for (int i = 0; i < commands.length; i++) {
+
+			int[] arr1 = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+			Arrays.sort(arr1);
+			answer[i] = arr1[commands[i][2]-1];
+		}
+
 		return answer;
 	}
 
